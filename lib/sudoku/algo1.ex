f3f71a84f95f652cdfc9053f29f6b6dbb9db727b
BenNG <benjamin.kim.nguyen@gmail.com>
2016-07-29 11:09:25 +0200

    feat(back/algo1) initialization of boxes SUD-3

--- lib/sudoku/algo1.ex
+++ lib/sudoku/algo1.ex
@@ -38,9 +38,9 @@ defmodule Sudoku.Algo1 do
   generate all the cols coordinates
       [{0,0},{0,1},{0,2},{0,3},{0,4},{0,5},{0,6},{0,7},{0,8},],...
   """
-  def create_cols do
-    Enum.reduce(0..8, [], fn(abs,acc) ->
-      col = Enum.reduce(0..8, [], fn(ord, accu) ->
+  def create_cols(size \\ 9) do
+    Enum.reduce(0..size - 1, [], fn(abs,acc) ->
+      col = Enum.reduce(0..size - 1, [], fn(ord, accu) ->
         [{abs,ord}|accu]
       end)
       |> Enum.reverse
@@ -53,9 +53,9 @@ defmodule Sudoku.Algo1 do
   generate all the rows coordinates
       [{0,0},{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0},{8,0},],...
   """
-  def create_rows do
-    Enum.reduce(0..8, [], fn(ord,acc) ->
-      col = Enum.reduce(0..8, [], fn(abs, accu) ->
+  def create_rows(size \\ 9) do
+    Enum.reduce(0..size - 1, [], fn(ord,acc) ->
+      col = Enum.reduce(0..size - 1, [], fn(abs, accu) ->
         [{abs,ord}|accu]
       end)
       |> Enum.reverse
@@ -63,5 +63,26 @@ defmodule Sudoku.Algo1 do
     end)
     |> Enum.reverse
   end
+
+  @doc """
+  generate all the boxes coordinates
+      [{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}],
+  """
+  def create_boxes(size \\ 3) do
+    range = Enum.slice([0..2, 3..5, 6..8], 0..size - 1 )
+
+    Enum.reduce(range, [], fn(r_ord,acc) ->
+      col_boxes = Enum.reduce(range, [], fn(r_abs, accu) ->
+        comp = for x <- r_abs,
+                   y <- r_ord,
+                   do: {x,y}
+        [comp | accu]
+      end)
+      |> Enum.reverse
+      acc ++ col_boxes
+    end)
+  end
+
+
 
 end
