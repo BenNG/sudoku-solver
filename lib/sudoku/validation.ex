f071fbebbb930ee7eacade78a550f6d9af0bf7f1
BenNG <benjamin.kim.nguyen@gmail.com>
2016-08-15 11:14:59 +0200

    chore(back/validation) refact

--- lib/sudoku/validation.ex
+++ lib/sudoku/validation.ex
@@ -1,4 +1,21 @@
 defmodule Sudoku.Validation do
+
+  @doc """
+  check if the entire sudoku is valid
+  """
+  def is_valid?(map) do
+    bool = Enum.reduce_while(0..8, false, fn(n,acc) ->
+      if is_row_valid?(n, map) && is_column_valid?(n, map) && is_box_valid?(n, map) do
+        {:cont, true}
+      else
+        {:halt, false}
+      end
+    end)
+
+    bool &&
+    any_empty_values?(map) === false
+  end
+
   def any_empty_values?(map) do
     map
     |> Map.values
@@ -8,38 +25,16 @@ defmodule Sudoku.Validation do
   def is_complete?(map) do
     map
     |> Map.values
-    |> Enum.all?(fn(list) -> length(list) === 1 end)
+    |> Enum.all?(&(length(&1) === 1))
   end
-
-
 
   @doc """
   Check if a row is valid
   """
   def is_row_valid?(n, map) do
     coords = Sudoku.Board.generate_row(n)
-    values = map_to_list_of_fixed_values(map, coords)
+    values = Sudoku.DataStructureUtils.map_to_list_of_fixed_values(map, coords)
     is_valid_unit?(values)
-  end
-
-  def is_valid_unit?(list) do
-    Enum.sort(list) === Enum.dedup(Enum.sort(list))
-  end
-
-  def map_to_list_of_fixed_values(map, coords) do
-    map = Map.take(map, coords)
-    fixed_map = Sudoku.DataStructureUtils.filter_fixed_values(map)
-    values = Map.values(fixed_map)
-  end
-
-  # @doc """
-  # Check if all rows are valid
-  # """
-  def is_rows_valid?(map) do
-    [true] === Enum.map(0..8, fn(n) ->
-      is_row_valid?(n, map)
-    end)
-    |> Enum.dedup
   end
 
   # @doc """
@@ -47,58 +42,21 @@ defmodule Sudoku.Validation do
   # """
   def is_column_valid?(n, map) do
     coords = Sudoku.Board.generate_column(n)
-    values = map_to_list_of_fixed_values(map, coords)
+    values = Sudoku.DataStructureUtils.map_to_list_of_fixed_values(map, coords)
     is_valid_unit?(values)
   end
-  #
-  # @doc """
-  # Check if all columns are valid
-  # """
-  def is_columns_valid?(map) do
-    [true] === Enum.map(0..8, fn(n) ->
-      is_column_valid?(n, map)
-    end)
-    |> Enum.dedup
-  end
-  #
-  # @doc """
-  # Check if a box is valid
-  # """
+
   def is_box_valid?(n, map) do
     coords = Sudoku.Board.generate_box(n)
-    values = map_to_list_of_fixed_values(map, coords)
+    values = Sudoku.DataStructureUtils.map_to_list_of_fixed_values(map, coords)
     is_valid_unit?(values)
   end
-  @doc """
-  # Check if all boxes are valid
-  """
-  def is_boxes_valid?(map) do
-    [true] === Enum.map(0..8, fn(n) ->
-      is_box_valid?(n, map)
-    end)
-    |> Enum.dedup
-  end
 
   @doc """
-  check if the entire sudoku is valid
+  return true if the unit (row, column, box) has no duplicate it may or may not be complete though
   """
-  def is_valid?(map) do
-    rows = Enum.map(0..8, fn(n) ->
-      is_row_valid?(n, map)
-    end)
-
-    cols = Enum.map(0..8, fn(n) ->
-      is_column_valid?(n, map)
-    end)
-
-    boxes = Enum.map(0..8, fn(n) ->
-      is_box_valid?(n, map)
-    end)
-
-    [true] === Enum.dedup(rows) &&
-    [true] === Enum.dedup(cols) &&
-    [true] === Enum.dedup(boxes) &&
-    any_empty_values?(map) === false
-
+  def is_valid_unit?(list) do
+    Enum.sort(list) === Enum.dedup(Enum.sort(list))
   end
+
 end
