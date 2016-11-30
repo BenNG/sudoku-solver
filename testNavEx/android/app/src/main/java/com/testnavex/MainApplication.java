32f412ca93fddc55aafd47d914f1fb346fce760f
BenNG <benjamin.kim.nguyen@gmail.com>
2016-11-30 10:46:48 +0100

    update to react native 0.38 et react 15.4

--- testNavEx/android/app/src/main/java/com/testnavex/MainApplication.java
+++ testNavEx/android/app/src/main/java/com/testnavex/MainApplication.java
@@ -2,7 +2,6 @@ package com.testnavex;
 
 import android.app.Application;
 import android.util.Log;
-
 import com.dylanvann.cameraroll.CameraRollPackage;
 
 import com.facebook.react.ReactApplication;
@@ -11,6 +10,7 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
@@ -35,6 +35,12 @@ public class MainApplication extends Application implements ReactApplication {
 
   @Override
   public ReactNativeHost getReactNativeHost() {
-      return mReactNativeHost;
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
   }
 }
