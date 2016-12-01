d9164e201eb8e90c39a7e887d12788ebaf06ae91
BenNG <benjamin.kim.nguyen@gmail.com>
2016-12-01 11:43:48 +0100

    use native toolbar

--- testNavEx/src/containers/header.js
+++ testNavEx/src/containers/header.js
@@ -14,29 +14,18 @@ export default class Header extends Component {
         this.onIconClicked = this.onIconClicked.bind(this);
     }
     render() {
-
         const { leftIcon } = this.props;
         return (
-            <View style={styles.container}>
-                <View style={{flexDirection: "row", alignItems: "center"}}>
-                    <Icon onPress={leftIcon.onPress} name="bars" size={20} color="#000" style={styles.leftIcon} />
-                    <Text>{this.props.title}</Text>
-                </View>
-            </View>
+            <ToolbarAndroid
+                navIcon={require("./menu.png")}
+                title={this.props.title}
+                // subtitle="ezaeza"
+                // subtitleColor= "rgba(0,0,0,0.4)"
+                onIconClicked={leftIcon.onPress}
+                style={[styles.toolbar, this.props.styles]}>
+            </ToolbarAndroid>
         );
     }
-    // render() {
-    //     console.log(this.props)
-    //     return (
-    //         <ToolbarAndroid
-    //             navIcon={require("./menu.png")}
-    //             logo={require("./emoticon.png")}
-    //             title="Toolbar"
-    //             onIconClicked={this.onIconClicked}
-    //             style={[styles.toolbar, this.props.styles]}>
-    //         </ToolbarAndroid>
-    //     );
-    // }
     onIconClicked() {
         console.log("onIconClicked");
         this.props.openDrawerFn();
@@ -45,17 +34,9 @@ export default class Header extends Component {
     }
 }
 
-
-const styles = StyleSheet.create({
-    container: {
-        flexDirection: "row",
-        // justifyContent: "space-between",
-        // backgroundColor: "red",
-        height: 46,
-    },
-    leftIcon: {
-        // backgroundColor: "green",
-        paddingVertical: 10,
-        paddingHorizontal: 15,
-    },
+var styles = StyleSheet.create({
+  toolbar: {
+    backgroundColor: '#e9eaed',
+    height: 56,
+  },
 });
