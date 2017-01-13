f8e935525d71656b6927aa77d6b7b75f2add8227
BenNG <benjamin.kim.nguyen@gmail.com>
2017-01-13 16:28:53 +0100

    delete mobile app

--- testNavEx/src/actions/gallery.js
+++
@@ -1,8 +0,0 @@
-import * as types from './types';
-
-export function selectedItem(uri) {
-    return {
-        type: types.SELECTED_ITEM,
-        uri,
-    };
-}
--- testNavEx/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import * as navigationActions from './navigation';
-import * as galleryActions from './gallery';
-import * as drawerActions from './drawer';
-
-export const ActionCreators = Object.assign({},
-  navigationActions,
-  galleryActions,
-  drawerActions,
-);
